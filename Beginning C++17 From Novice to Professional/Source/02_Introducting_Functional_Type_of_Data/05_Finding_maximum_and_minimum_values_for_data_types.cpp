f0bd5cf1695f2a8026eab875507b37ec84c28a25
Gunho Park <devgun@kakao.com>
2021-08-29 16:16:27 +0900

    Add 02-05

---
+++ Beginning C++17 From Novice to Professional/Source/02_Introducting_Functional_Type_of_Data/05_Finding_maximum_and_minimum_values_for_data_types.cpp
@@ -0,0 +1,33 @@
+// Finding maximum and minimum values for data types
+
+#include <limits>
+#include <iostream>
+
+int main()
+{
+    std::cout << "The range for type short is from "
+              << std::numeric_limits<short>::min() << " to "
+              << std::numeric_limits<short>::max() << std::endl;
+
+    std::cout << "The range for type int is from "
+              << std::numeric_limits<int>::min() << " to "
+              << std::numeric_limits<int>::max() << std::endl;
+
+    std::cout << "The range for type long is from "
+              << std::numeric_limits<long long>::min() << " to "
+              << std::numeric_limits<long long>::max() << std::endl;
+
+    std::cout << "The range for type float is from "
+              << std::numeric_limits<float>::min() << " to "
+              << std::numeric_limits<float>::max() << std::endl;
+
+    std::cout << "The positive range for type double is from "
+              << std::numeric_limits<double>::min() << " to "
+              << std::numeric_limits<double>::max() << std::endl;
+
+    std::cout << "The positive range for type long double is from "
+              << std::numeric_limits<long double>::min() << " to "
+              << std::numeric_limits<long double>::max() << std::endl;
+
+    return 0;
+}
