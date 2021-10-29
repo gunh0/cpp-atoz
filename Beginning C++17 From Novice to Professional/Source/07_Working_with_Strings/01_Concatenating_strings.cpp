198a1dcc026b3650a0a7ad7a775724b9b6dbdb67
Gunho Park <devgun@kakao.com>
2021-10-29 20:42:55 +0900

    add: cpp17 pro, 07-01

---
+++ Beginning C++17 From Novice to Professional/Source/07_Working_with_Strings/01_Concatenating_strings.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    std::string first;
+    std::string second;
+
+    std::cout << "Enter your first name: ";
+    std::cin >> first;
+
+    std::cout << "Enter your second name: ";
+    std::cin >> second;
+
+    std::string sentence{"Your full name is "};
+    sentence += first + " " + second + ".";
+    std::cout << sentence << std::endl;
+    std::cout << "The string contains " << sentence.length() << " charactoers." << std::endl;
+}
